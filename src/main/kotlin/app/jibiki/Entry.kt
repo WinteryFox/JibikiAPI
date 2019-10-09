a8d648f44119370d2585f3f316a35403502f95fc
WinteryFox <dragonhel5@gmail.com>
2019-10-09 20:45:56 +0200

    english and japanese search; forms sorted by frequency

--- src/main/kotlin/app/jibiki/Entry.kt
+++ src/main/kotlin/app/jibiki/Entry.kt
@@ -2,7 +2,6 @@ package app.jibiki
 
 data class Entry(
         val kanji: String,
-        val reading: String,
-        val forms: Array<String?>,
-        val glossary: Array<String?>
+        val forms: Array<String>,
+        val glossary: Array<String>
 )
--- src/main/kotlin/app/jibiki/QueryController.kt
+++ src/main/kotlin/app/jibiki/QueryController.kt
@@ -8,13 +8,17 @@ import org.springframework.web.bind.annotation.*
 class QueryController {
     @RequestMapping(method = [RequestMethod.GET], value = ["/"], produces = ["application/json"])
     fun query(@RequestParam(value = "q") query: String): Array<Entry> {
-        return database.query("SELECT kanj.entr entry, kanj.txt kanji, treading.txt reading, array_agg(tforms.txt) forms, array_agg(tglossary.txt) glossary FROM kanj\n" +
-                "    INNER JOIN rdng treading ON (kanj.entr = treading.entr AND treading.rdng = 1)\n" +
-                "    LEFT JOIN gloss tglossary ON (kanj.entr = tglossary.entr AND lang = 1)\n" +
-                "    LEFT JOIN rdng tforms ON (kanj.entr = tforms.entr AND tforms.rdng > 1)\n" +
-                "WHERE (SELECT src FROM entr WHERE id = kanj.entr) = 1 AND (kanj.txt LIKE ? OR tglossary.txt LIKE ?)\n" +
-                "GROUP BY entry, kanji, reading", query.replace('*', '%'), query.replace('*', '%'))
-                .map { Entry(it.get("kanji"), it.get("reading"), it.get<java.sql.Array>("forms").array as Array<String?>, it.get<java.sql.Array>("glossary").array as Array<String?>) }
+        return database.query("SELECT kanj.txt           kanji,\n" +
+                "       tforms.forms       forms,\n" +
+                "       tglossary.glossary glossary\n" +
+                "FROM kanj\n" +
+                "         JOIN (SELECT entr, array_agg(rdng), array_agg(txt ORDER BY rdng) AS forms FROM rdng GROUP BY 1) tforms\n" +
+                "              ON tforms.entr = kanj.entr\n" +
+                "         JOIN (SELECT entr, array_agg(txt) AS glossary FROM gloss GROUP BY 1) tglossary ON tglossary.entr = kanj.entr\n" +
+                "WHERE (SELECT src FROM entr WHERE id = kanj.entr) = 1\n" +
+                "  AND (kanj.txt LIKE ? OR lower(?) ILIKE ANY(glossary))\n" +
+                "GROUP BY kanji, tforms.forms, tglossary.glossary;", query.replace('*', '%'), query.replace('*', '%'))
+                .map { Entry(it.get("kanji"), it.get<java.sql.Array>("forms").array as Array<String>, it.get<java.sql.Array>("glossary").array as Array<String>) }
                 .toTypedArray()
     }
 
