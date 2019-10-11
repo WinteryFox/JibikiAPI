8ab5dc427b1ff665e5254986bb2313b56c917554
WinteryFox <dragonhel5@gmail.com>
2019-10-11 12:52:29 +0200

    progress

--- src/main/kotlin/app/jibiki/ApiController.kt
+++ src/main/kotlin/app/jibiki/ApiController.kt
@@ -1,30 +1,36 @@
 package app.jibiki
 
+import com.moji4j.MojiConverter
+import com.moji4j.MojiDetector
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.MissingServletRequestParameterException
 import org.springframework.web.bind.annotation.*
 
 @RestController
 class ApiController {
-    @RequestMapping(method = [RequestMethod.GET], value = ["/api/v1/word/{word}"], produces = ["application/json"])
-    fun query(@PathVariable("word") query: String): Array<Entry> {
+    @RequestMapping(method = [RequestMethod.GET], value = ["/api/v1/word"], produces = ["application/json"])
+    fun query(@RequestParam("q") query: String): Array<Entry> {
         val word = query.replace('*', '%')
 
-        val entry = database.query("SELECT id, kanji.kanji, reading.reading\n" +
-                "FROM entr\n" +
-                "         LEFT JOIN (SELECT entr, array_agg(txt ORDER BY kanj) kanji FROM kanj GROUP BY entr) kanji\n" +
-                "                   ON kanji.entr = entr.id\n" +
-                "         LEFT JOIN (SELECT entr, array_agg(txt ORDER BY rdng) reading FROM rdng GROUP BY entr) reading\n" +
-                "                   ON reading.entr = entr.id\n" +
-                "WHERE src = 1\n" +
-                "  AND (id = ANY (SELECT entr FROM rdng WHERE txt ILIKE ?)\n" +
-                "    OR id = ANY (SELECT entr FROM kanj WHERE txt ILIKE ?)\n" +
-                "    OR id = ANY (SELECT entr FROM gloss WHERE txt ILIKE ?));", word, word, word)
+        val detector = MojiDetector()
+        val converter = MojiConverter()
+        val entries = when {
+            detector.hasKanji(query) -> database.query("SELECT entr FROM kanj WHERE txt ILIKE ?", word)
+            detector.hasKana(query) -> database.query("SELECT entr FROM rdng WHERE txt ILIKE ?", word)
+            else -> {
+                val kana = database.query("SELECT entr FROM rdng WHERE txt ILIKE ?", converter.convertRomajiToHiragana(word))
+                if (kana.isNotEmpty())
+                    kana
+                else
+                    database.query("SELECT entr FROM gloss WHERE txt ILIKE ?", word)
+            }
+        }
+
         val sense = database.query("SELECT s.entr                                   entry,\n" +
                 "       s.sens                                   sense,\n" +
-                "       STRING_AGG(DISTINCT kwpos.kw, ',')    as pos,\n" +
-                "       STRING_AGG(DISTINCT kwfld.descr, ',') as fld,\n" +
-                "       STRING_AGG(g.txt, ',')                as gloss\n" +
+                "       ARRAY_AGG(DISTINCT kwpos.kw)    as pos,\n" +
+                "       ARRAY_AGG(DISTINCT kwfld.descr) as fld,\n" +
+                "       ARRAY_AGG(g.txt)                as gloss\n" +
                 "FROM sens s\n" +
                 "         LEFT JOIN pos p ON p.entr = s.entr AND p.sens = s.sens\n" +
                 "         LEFT JOIN kwpos ON kwpos.id = p.kw\n" +
@@ -32,19 +38,19 @@ class ApiController {
                 "         LEFT JOIN kwfld ON kwfld.id = f.kw\n" +
                 "         LEFT JOIN gloss g ON g.entr = s.entr AND g.sens = s.sens\n" +
                 "WHERE s.entr = ANY (?)\n" +
-                "GROUP BY s.entr, s.sens;", entry.map { it.get<Long>("id") }.toLongArray())
+                "GROUP BY s.entr, s.sens;", entries.map { it.get<Long>("entr") }.toLongArray())
 
-        return entry.map { row ->
+        return entries.map { row ->
             Entry(
                     row.get<java.sql.Array>("kanji")?.array as Array<String>?,
                     row.get<java.sql.Array>("reading")?.array as Array<String>?,
                     sense
-                            .filter { it.get<Int>("entry") == row.get<Int>("id") }
+                            .filter { it.get<Int>("entry") == row.get<Int>("entr") }
                             .map {
                                 Sense(
-                                    it.get<java.sql.Array>("gloss")?.array as Array<String>?,
-                                    it.get<java.sql.Array>("kw")?.array as Array<String>?,
-                                    it.get<java.sql.Array>("fld")?.array as Array<String>?
+                                        it.get<java.sql.Array>("gloss")?.array as Array<String>?,
+                                        it.get<java.sql.Array>("pos")?.array as Array<String>?,
+                                        it.get<java.sql.Array>("fld")?.array as Array<String>?
                                 )
                             }
                             .toTypedArray())
