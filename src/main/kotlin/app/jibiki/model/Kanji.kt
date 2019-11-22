a6dd37bac06409651020512eaba2cec19e616dd7
ByteAlex <bytealex@zerotwo.bot>
2019-11-22 14:33:10 +0100

    Manual merge from master

--- src/main/kotlin/app/jibiki/model/Kanji.kt
+++ src/main/kotlin/app/jibiki/model/Kanji.kt
@@ -3,7 +3,7 @@ package app.jibiki.model
 data class Kanji(
         val id: Short = 0,
         val literal: String = "",
-        val meaning: Array<String> = emptyArray(),
+        val meaning: Array<String>? = null,
         val kunyomi: Array<String>? = null,
         val onyomi: Array<String>? = null,
         val grade: Int? = null,
